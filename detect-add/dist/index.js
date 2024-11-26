43d3fee7fb58eac2b45d798aca5e5b97901dbfb6
oneweek-lee <oneweek.lee@navercorp.com>
2024-11-26 19:20:52 +0900

    [#13] 본문 띄어쓰기 및 문구 변경

--- detect-add/dist/index.js
+++ detect-add/dist/index.js
@@ -33978,10 +33978,9 @@ function getNewChangesetTemplate(changedPackageNames, title, prUrl, versionType)
         ...changedPackageNames.map((x) => `"${x}": ${versionType}`),
         '---',
         '',
+        `${title}`,
         '',
-        '<!-- 변경된 사항을 입력해주세요. (이 줄을 지우고 하단에 명확하게 작성해주세요.) -->',
-        '',
-        `[${title}](${prUrl})`,
+        `PR: [${title}](${prUrl})`,
     ].join('\n');
     return encodeURIComponent(contents);
 }
