e8334f1a7d0ae3d30caaf6cdf371046539ddd281
yceffort <yc.effort@navercorp.com>
2025-11-17 15:54:50 +0900

    🔒 Support OIDC authentication for npm publishing

--- canary-publish/src/utils/npm.ts
+++ canary-publish/src/utils/npm.ts
@@ -2,9 +2,15 @@ import * as core from '@actions/core'
 import * as fs from 'fs-extra'
 
 export async function setNpmRc() {
-    // 입력값에서 npm_token 가져오기
     const npmToken = core.getInput('npm_token')
-    process.env.NPM_TOKEN = npmToken // 환경변수로 설정
+
+    if (!npmToken) {
+        core.info('No npm_token provided - assuming OIDC trusted publishing is configured')
+        core.info('OIDC authentication will be used automatically by npm CLI 11.5.1+')
+        return
+    }
+
+    process.env.NPM_TOKEN = npmToken
 
     core.info('No changesets found, attempting to publish any unpublished packages to npm')
 
@@ -13,7 +19,6 @@ export async function setNpmRc() {
         core.info('Found existing user .npmrc file')
         const userNpmrcContent = await fs.readFile(userNpmrcPath, 'utf8')
         const authLine = userNpmrcContent.split('\n').find((line) => {
-            // check based on https://github.com/npm/cli/blob/8f8f71e4dd5ee66b3b17888faad5a7bf6c657eed/test/lib/adduser.js#L103-L105
             return /^\s*\/\/registry\.npmjs\.org\/:[_-]authToken=/i.test(line)
         })
         if (authLine) {
--- publish/src/utils/npm.ts
+++ publish/src/utils/npm.ts
@@ -2,16 +2,21 @@ import * as core from '@actions/core'
 import * as fs from 'fs-extra'
 
 export async function setNpmRc() {
-    core.info('No changesets found, attempting to publish any unpublished packages to npm')
-
     const npmToken = core.getInput('npm_token')
+
+    if (!npmToken) {
+        core.info('No npm_token provided - assuming OIDC trusted publishing is configured')
+        core.info('OIDC authentication will be used automatically by npm CLI 11.5.1+')
+        return
+    }
+
+    core.info('No changesets found, attempting to publish any unpublished packages to npm')
 
     const userNpmrcPath = `${process.env.HOME}/.npmrc`
     if (fs.existsSync(userNpmrcPath)) {
         core.info('Found existing user .npmrc file')
         const userNpmrcContent = await fs.readFile(userNpmrcPath, 'utf8')
         const authLine = userNpmrcContent.split('\n').find((line) => {
-            // check based on https://github.com/npm/cli/blob/8f8f71e4dd5ee66b3b17888faad5a7bf6c657eed/test/lib/adduser.js#L103-L105
             return /^\s*\/\/registry\.npmjs\.org\/:[_-]authToken=/i.test(line)
         })
         if (authLine) {
